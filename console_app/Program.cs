e0d8069de61a9df2ee99229d14549e1f96ae1872
Poorvi Prajapati <poorvi.prajapati@agreeya.net>
2025-08-12 13:03:31 +0530

    first push

---
+++ console_app/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using System;
+
+Console.WriteLine("Welcome to the .NET Console Fun App!");
+
+// Ask for name
+Console.Write("What's your name? ");
+string name = Console.ReadLine();
+Console.WriteLine($"Nice to meet you, {name}!");
+
